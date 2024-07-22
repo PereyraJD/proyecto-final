8a8cb7db8d0b7955b0bd5bbad1b2cfe3f4524db8
FrancisEncarnacion <francisencarnacion082@gmail.com>
2024-07-22 14:54:25 -0700

    Agregando conexion al cluster

---
+++ src/db/dbconnection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const host = 'localhost'
+const port = '27017'
+const database = 'pelisApp'
+const url = `mongodb+srv://fencarnacion1505:Francis1234_@cluster0.4ytiagc.mongodb.net/${database}retryWrites=true&w=majority&appName=Cluster0`
+
+const dbconnection = async () => {
+    try {
+        await mongoose.connect(url)
+        console.log(`conectado a la base de datos`)
+    } catch (error) {
+        console.log("Error al conectar la base de datos: " + error)
+    }
+}
+dbconnection()
+module.exports 
