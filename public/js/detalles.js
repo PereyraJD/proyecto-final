09a4b41a486a9ebc49ae91db023a031330e37cb0
Marien <marienmb8@gmail>
2024-07-30 01:46:28 -0400

    Actualizaciones de la pagina detalles

--- public/js/detalles.js
+++ public/js/detalles.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('/random-movie')
-        .then(response => response.json())
-        .then(movie => {
-            // Actualiza detalles de la película
-            document.getElementById('title').textContent = movie.titulo;
-            document.getElementById('synopsis').textContent = movie.sinopsis;
-            document.getElementById('genre').textContent = `Género: ${movie.genero}`;
-            document.getElementById('duration').textContent = `Duración: ${movie.duracion}`;
-            document.getElementById('rating').textContent = `Clasificación por edad: ${movie.clasificacion_por_edad}`;
-            document.getElementById('cast').textContent = `Elenco: ${movie.elenco.join(', ')}`;
-            document.getElementById('director').textContent = `Director: ${movie.director}`;
-            document.getElementById('year').textContent = `Año de lanzamiento: ${movie.año_de_lanzamiento}`;
-            document.getElementById('poster').src = movie.portada;
-            document.getElementById('trailer').href = movie.trailer;
+const insertarElementos = (movie) => {
 
-            // Actualiza el contenedor del tráiler
-            const trailerContainer = document.getElementById('trailer-container');
-            const trailerVideo = document.getElementById('trailer-video');
-            const youtubeLink = document.getElementById('youtube-link');
+document.getElementById("title").textContent = movie.titulo;
+document.getElementById("synopsis").textContent = movie.sinopsis;
+document.getElementById("genre").textContent = `Género: ${movie.genero}`;
+document.getElementById("duration").textContent = `Duración: ${movie.duracion}`;
+document.getElementById(
+  "rating"
+).textContent = `Clasificación por edad: ${movie.clasificacion_por_edad}`;
+document.getElementById("cast").textContent = `Elenco: ${movie.elenco.join(
+  ", "
+)}`;
+document.getElementById("director").textContent = `Director: ${movie.director}`;
+document.getElementById(
+  "year"
+).textContent = `Año de lanzamiento: ${movie.año_de_lanzamiento}`;
+document.getElementById("poster").src = movie.portada;
+document.getElementById("trailer").href = movie.trailer;
+}
 
-            if (movie.trailer) {
-                trailerVideo.src = movie.trailer.replace('watch?v=', 'embed/');
-                youtubeLink.href = movie.trailer;
-            } else {
-                trailerContainer.innerHTML = '<p>Tráiler no disponible</p>';
-                youtubeLink.style.display = 'none';
-            }
-        })
-        .catch(error => console.error('Error fetching movie data:', error));
-});
+let trailer = ''
+
+const obtenerDatos = async () => { 
+  const url = "http://localhost:3000/movies/66a16a66f8d3ab06537c865d";
+  const response = await fetch(url);
+  const movie = await response.json();
+  console.log(movie);
+  insertarElementos(movie)
+  trailer = await movie.trailer
+};
+
+obtenerDatos();
+console.log(trailer);
 
