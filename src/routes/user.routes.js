a6824acae5dcc118e6c3bd58775822703b6c7df8
FrancisEncarnacion <francisencarnacion082@gmail.com>
2024-08-01 11:13:07 -0400

    Añadiendo controladores y rutas de los usuarios

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const controllers = require('../controllers/user.controllers')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -7,6 +8,14 @@ router.post('/register', controllers.registerUser)
 
 router.post('/login', controllers.userLogin)
 
+router.get('/get',auth, controllers.getUsers)
+
+router.get('/get/:id',auth, controllers.getUser)
+
+router.patch('/update/:id',auth, controllers.updateUser)
+
+router.delete('/delete/:id',auth, controllers.deleteUser)
 
 
-module.exports = router
+
+module.exports = router 
